359f40432510282e4de3b92468a6646858a3e8eb
Betoml5 <betoml5@hotmail.com>
2020-11-29 23:57:42 -0600

    I added sass cuz fuck yeah

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const $sections = document.getElementsByClassName('section');
 const $sectionText = document.getElementsByClassName('section__text');
 const $images = document.getElementsByClassName('image');
+const $section_h3 = document.getElementsByClassName('section__container-h3');
 
 
 
@@ -9,6 +10,7 @@ for(let i = 0; i < $sections.length; i++){
     $sections[i].addEventListener('click', () => {
         $sectionText[i].classList.toggle('hide')
         $images[i].classList.toggle('rotate')
+        $section_h3[i].classList.toggle('strong');
     })
 }
 
