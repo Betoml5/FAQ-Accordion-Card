3938ddbeacf8aeb8cdbdb18a4681d1f485e4aef3
Betoml5 <betoml5@hotmail.com>
2020-12-02 13:11:30 -0600

    Mobile done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-const $sections = document.getElementsByClassName('section');
-const $sectionText = document.getElementsByClassName('section__text');
-const $images = document.getElementsByClassName('image');
-const $section_h3 = document.getElementsByClassName('section__container-h3');
+const $sections = document.getElementsByClassName('main__questions-item');
+const $sectionText = document.getElementsByClassName('main__question-item-hidden');
+const $images = document.getElementsByClassName('arrowDown-image');
+const $section_h3 = document.getElementsByClassName('main-question-item-h3');
 
 
 
 
 for(let i = 0; i < $sections.length; i++){
     $sections[i].addEventListener('click', () => {
-        $sectionText[i].classList.toggle('hide')
+        $sectionText[i].classList.toggle('main__question-item-show')
         $images[i].classList.toggle('rotate')
         $section_h3[i].classList.toggle('strong');
     })
